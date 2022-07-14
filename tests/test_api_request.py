f0146b9b93671e6034c789b78e9ad1e0b37a99f9
alec sharp <alecsharpie@gmail.com>
2022-07-14 21:02:45 +1000

    break test

--- tests/test_api_request.py
+++ tests/test_api_request.py
@@ -3,7 +3,7 @@ from bbquote852.api_request import get_quote
 print('2nd: ', get_quote())
 
 def test_type_get_quote():
-    assert type(get_quote()) == str
+    assert type(get_quote()) == int
 
 def test_length_get_quote():
     assert len(get_quote()) > 0
